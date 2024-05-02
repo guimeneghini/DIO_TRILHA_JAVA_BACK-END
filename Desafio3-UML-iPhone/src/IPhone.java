a87268906dbce52ae4072535f17aaa9c840abbf9
guimeneghini <guimeneghini10@gmail.com>
2024-05-02 13:40:55 -0300

    organização e desafio UML do iPhone

---
+++ Desafio3-UML-iPhone/src/IPhone.java
@@ -0,0 +1,46 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    @Override
+    public void tocar() {
+        // Método para tocar música
+    }
+
+    @Override
+    public void pausar() {
+        // Método para pausar música
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // Método para selecionar uma música
+    }
+
+    @Override
+    public void ligar(String numero) {
+        // Método para realizar uma ligação
+    }
+
+    @Override
+    public void atender() {
+        // Método para atender uma chamada
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Método para iniciar correio de voz
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        // Método para exibir uma página da web
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        // Método para adicionar uma nova aba no navegador
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Método para atualizar a página no navegador
+    }
+}
